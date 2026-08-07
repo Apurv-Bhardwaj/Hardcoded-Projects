86a7de0457696b370b01ae17f29a09d5ded4cd73
Apurv Bhardwaj <apurvbhardwaj24@gmail.com>
2026-08-07 14:28:11 +0530

    edit profile page added for users to add aboutme and username

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,7 @@ from flask_login import login_required
 from flask import request
 from urllib.parse import urlsplit
 from app.forms import RegistrationForm
+from datetime import datetime, timezone
 
 @app.route("/")
 @app.route('/index')
@@ -74,3 +75,10 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+        
