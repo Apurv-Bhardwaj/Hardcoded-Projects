c5f086b72d62add2b31e8705af8823d0e031c90d
Apurv Bhardwaj <apurvbhardwaj24@gmail.com>
2026-08-07 13:30:18 +0530

    Refactored user registration flow and improved error handling

--- app/models.py
+++ app/models.py
@@ -26,8 +26,11 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.password_hash, password)
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return f'http://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'   
-
+        return f'http://www.gravatar.com/avatar/{digest}?d=identicon&s={size}' 
+         
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
+    
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
