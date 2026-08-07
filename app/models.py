41bdd07c3ac7c06667f62997ef758ae099a49094
Apurv Bhardwaj <apurvbhardwaj24@gmail.com>
2026-08-07 23:17:20 +0530

    Inititated code for follower and following for the purpose of the blog.

--- app/models.py
+++ app/models.py
@@ -31,6 +31,14 @@ class User(UserMixin,db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     
+    following: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates='followers')
+    followers: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates='following')
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -45,3 +53,12 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
+
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
+               primary_key=True),
+    sa.Column('Followed_id', sa.Integer, sa.ForeignKey('user.id'),
+                primary_key=True)
+)
